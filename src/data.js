6b070987222f1c87008fc23de6f65030aa245d3e
oli-mac <adumulugeta21@gmail.com>
2021-11-16 14:58:55 +0300

    commit for the product

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "p1.jpg",
+      link: "http://olyadmulugeta.me",
+    },
+    {
+      id: 2,
+      img: 'p2.jpg',
+      link: "http://olyadmulugeta.me",
+    },
+    {
+      id: 3,
+      img: "./images/p3.jpg",
+      link: "http://olyadmulugeta.me",
+    },
+    {
+      id: 4,
+      img: "./images/p4.jpg",
+      link: "http://olyadmulugeta.me",
+    },
+    {
+      id: 5,
+      img: "./images/p5.jpg",
+      link: "http://olyadmulugeta.me",
+    },
+    {
+      id: 6,
+      img: "./images/p1.jpg",
+      link: "http://olyadmulugeta.me",
+    },
+  ];
